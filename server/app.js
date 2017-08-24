ea43c54d3b8ca29b1fd8c6b7109efc1d20a6b162
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-08-24 11:59:54 +0200

    pugs and data

--- server/app.js
+++ server/app.js
@@ -4,7 +4,13 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
+app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, '../client')))
+
+app.get('/', (req, res) => {
+  const posts = require('./data/posts.json')
+  res.render('home', { posts })
+})
 
 app.listen(PORT)
 console.log(`Magic happens at PORT ${PORT}...`)
